cf95ce8c0174eb05c8a3eea806830c571b1be75c
vgx6688 <vgx6688@autuni.ac.nz>
2022-05-27 13:39:49 +1200

    Update View Class
    Added JFrame components

--- PDC_Project2/src/Show_Ticket_Program/View.java
+++ PDC_Project2/src/Show_Ticket_Program/View.java
@@ -5,10 +5,45 @@
  */
 package Show_Ticket_Program;
 
+import java.util.Observable;
+import java.util.Observer;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 /**
  *
  * @author JL Lopez
  */
-public class View {
+public class View extends JFrame implements Observer {
+
+    private JPanel infoPanel = new JPanel();
+    private JLabel showID = new JLabel("Show ID: ");
+    private JLabel date = new JLabel("Date: ");
+    private JLabel goldTicks = new JLabel("Gold Tickets left: ");
+    private JLabel silverTicks = new JLabel("Silver Tickets left: ");
+    private JLabel bronzeTicks = new JLabel("Bronze Tickets left: ");
+
+    public View() {
+        
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,600);
+        this.setLocationRelativeTo(null);
+        infoPanel.add(showID);
+        infoPanel.add(date); 
+        infoPanel.add(goldTicks);
+        infoPanel.add(silverTicks);
+        infoPanel.add(bronzeTicks);
+        this.add(infoPanel); 
+        this.setVisible(true); 
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
+    public static void main(String[] args) {
+        View view = new View();
+    }
 }
