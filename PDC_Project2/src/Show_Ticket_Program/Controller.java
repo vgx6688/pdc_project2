dbe7fcc6a267331c0218cccfe10e0d660031e9d4
vgx6688 <vgx6688@autuni.ac.nz>
2022-05-31 23:09:39 +1200

    Update Controller.java

--- PDC_Project2/src/Show_Ticket_Program/Controller.java
+++ PDC_Project2/src/Show_Ticket_Program/Controller.java
@@ -5,10 +5,39 @@
  */
 package Show_Ticket_Program;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author JL Lopez
  */
-public class Controller {
+public class Controller implements ActionListener {
+
+    public View view;
+    public Model model;
+
+    public Controller(View view, Model model) {
+        this.view = view;
+        this.model = model;
+        this.view.addActionListener(this);
+    }
     
+    
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+        switch (command) {
+            case "Confirm":
+                int confirm = JOptionPane.showConfirmDialog(view, "Please double check inputs then press Yes to confirm.", "Booking confirmation", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+                break;
+            case "Cancel":
+                int cancel = JOptionPane.showConfirmDialog(view, "Do you want to cancel booking session?", "Cancel Confirmation", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+                break;
+            default:
+                break;
+        }
+    }
 }
