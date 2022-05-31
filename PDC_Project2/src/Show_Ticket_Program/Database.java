e5b0dea88a8dcab781e0bbcea186b0cae0c3a763
vgx6688 <vgx6688@autuni.ac.nz>
2022-05-31 20:51:23 +1200

    Fixed up Database class

--- PDC_Project2/src/Show_Ticket_Program/Database.java
+++ PDC_Project2/src/Show_Ticket_Program/Database.java
@@ -11,8 +11,6 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -26,18 +24,22 @@ public class Database {
     String dbpassword = "pdc";
 
     public void dbsetup() {
-        this.createTable("ShowsInfo");
-        this.createTable("TicketsInfo");
-        this.createTable("ShowsABookings");
-        this.createTable("ShowsBBookings");
-        this.createTable("ShowsCBookings");
+        try{
+            conn = DriverManager.getConnection(url, dbusername, dbpassword);
+            this.createTable("ShowsInfo");
+            this.createTable("TicketsInfo");
+            this.createTable("ShowsABookings");
+            this.createTable("ShowsBBookings");
+            this.createTable("ShowsCBookings");
+        } catch(Throwable e){
+            System.out.println(e.getMessage());
+        }
+        
     }
 
     public void createTable(String tableName) {
         try {
-            conn = DriverManager.getConnection(url, dbusername, dbpassword);
             Statement statement = conn.createStatement();
-
             if (!checkTableExists(tableName)) {
                 if (tableName.equalsIgnoreCase("ShowsInfo")) {
                     statement.executeUpdate("CREATE TABLE " + tableName + " (showID VARCHAR(1), date DATE, goldticket INT, silverticket INT, bronzeticket INT)");
@@ -46,12 +48,12 @@ public class Database {
                     statement.executeUpdate("CREATE TABLE " + tableName + " (tickettype VARCHAR(10), price INT)");
                     this.insertTableData(tableName);
                 } else {
-                    statement.executeUpdate("CREATE TABLE " + tableName + " (bookingid VARCHAR(10), user VARCHAR(12), goldticket INT, silverticket INT, bronzeticket INT)");
+                    statement.executeUpdate("CREATE TABLE " + tableName + " (bookingid VARCHAR(10), name VARCHAR(12), goldticket INT, silverticket INT, bronzeticket INT)");
                 }
             }
             statement.close();
         } catch (Throwable e) {
-            System.out.println("ERROR");
+            System.out.println(e.getMessage());
         }
     }
 
@@ -72,51 +74,20 @@ public class Database {
                 statement.executeBatch();
             }  
         } catch(Throwable e){
-            System.out.println("ERROR");
+            System.out.println(e.getMessage());
         }
         
     }
-
-//    public Data checkName(String username, String password) {
-//        Data data = new Data();
-//        try {
-//            Statement statement = conn.createStatement();
-//            ResultSet rs = statement.executeQuery("SELECT userid, password, score FROM UserInfo WHERE userid = '" + username + "'");
-//            if (rs.next()) {
-//                String pass = rs.getString("password");
-//                System.out.println("***" + pass);
-//                System.out.println("found user");
-//
-//                if (password.compareTo(pass) == 0) {
-//                    data.currentScore = rs.getInt("score");
-//                    data.loginFlag = true;
-//                } else {
-//                    data.loginFlag = false;
-//                }
-//            } else {
-//                System.out.println("User not found");
-//                statement.executeUpdate("INSERT INTO UserInfo VALUES('" + username + "', '" + password + "', 0");
-//                data.currentScore = 0;
-//                data.loginFlag = true;
-//            }
-//        } catch (SQLException ex) {
-//            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//        return data;
-//    }
 
     public boolean checkTableExists(String tableName) {
         boolean exists = false;
         try {
             DatabaseMetaData dbmd = this.conn.getMetaData();
             String[] types = {"TABLE"};
-            Statement statement;
-            statement = this.conn.createStatement();
             ResultSet rs = dbmd.getTables(null, null, null, types);
 
             while (rs.next()) {
                 String table_name = rs.getString("TABLE_NAME");
-                System.out.println(table_name);
                 if (table_name.equalsIgnoreCase(tableName)) {
                     exists = true;
                 }
