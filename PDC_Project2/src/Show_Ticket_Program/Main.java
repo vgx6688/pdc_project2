802e87cb854d78a81c0537bc8da8d07a8003706f
vgx6688 <vgx6688@autuni.ac.nz>
2022-05-31 20:52:56 +1200

    Modified Main class
    Added lines of code for testing purposes

--- PDC_Project2/src/Show_Ticket_Program/Main.java
+++ PDC_Project2/src/Show_Ticket_Program/Main.java
@@ -16,6 +16,10 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+       // View view = new View();
+       // view.setVisible(true);
+        Model model = new Model();
+        //Controller controller = new Controller(view, model);
     }
     
 }
