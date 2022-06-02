72958f6faed5014f989d95e014f90899f34ee5c4
vgx6688 <vgx6688@autuni.ac.nz>
2022-06-02 15:27:07 +1200

    File Changes

---
+++ PDC_Project2/src/Show_Ticket_Program/UserData.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Show_Ticket_Program;
+
+/**
+ *
+ * @author JL Lopez
+ */
+public class UserData {
+    public String name; 
+    public  String phNum;
+    public String show; 
+    public int gold; 
+    public int silver; 
+    public int bronze; 
+    
+}
