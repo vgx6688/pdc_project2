9ba2b5cc1ed81192d4a1ec5c445a438471593421
vgx6688 <vgx6688@autuni.ac.nz>
2022-06-02 15:29:50 +1200

    Updated Controller
    Added cases for more Events from the View class

--- PDC_Project2/src/Show_Ticket_Program/Controller.java
+++ PDC_Project2/src/Show_Ticket_Program/Controller.java
@@ -7,6 +7,7 @@ package Show_Ticket_Program;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
 
 /**
@@ -32,12 +33,29 @@ public class Controller implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         String command = e.getActionCommand();
         switch (command) {
+            case "Show A":
+//                System.out.println("Show A Button selected");
+                model.updateBoxes("A");
+                break;
+            case "Show B":
+//                System.out.println("Show B Button selected");
+                model.updateBoxes("B");
+                break;
+            case "Show C":
+//                System.out.println("Show C Button selected");
+                model.updateBoxes("C");
+            case "Gold Ticket":
+                System.out.println("Gold ticket box has been changed");
+                JComboBox source = (JComboBox)e.getSource();
+                int numTickets = Integer.parseInt((String)source.getSelectedItem());
+                model.updateTotalCost("G", numTickets);
+                break;
             case "Confirm":
                 int confirm = JOptionPane.showConfirmDialog(view, "Please double check inputs then press Yes to confirm.", "Booking confirmation", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
                 break;
             case "Cancel":
                 int cancel = JOptionPane.showConfirmDialog(view, "Do you want to cancel booking session?", "Cancel Confirmation", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-                break;
+                break;  
             default:
                 break;
         }
