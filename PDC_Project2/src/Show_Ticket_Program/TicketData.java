0c0a722d1a6a1ec68ad2292c18c62b487466c5dc
vgx6688 <vgx6688@autuni.ac.nz>
2022-06-02 15:31:18 +1200

    Create TicketData.java

---
+++ PDC_Project2/src/Show_Ticket_Program/TicketData.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Show_Ticket_Program;
+
+/**
+ *
+ * @author JL Lopez
+ */
+public class TicketData {
+    public String type; 
+    public int quantity;
+    public double price; 
+    
+}
