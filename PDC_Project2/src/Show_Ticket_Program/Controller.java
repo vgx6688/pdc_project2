fc20bf47c4283784446ace6586993bdb7eee3224
vgx6688 <vgx6688@autuni.ac.nz>
2022-06-07 17:28:15 +1200

    Update Controller.java
    Added code for confirm button event

--- PDC_Project2/src/Show_Ticket_Program/Controller.java
+++ PDC_Project2/src/Show_Ticket_Program/Controller.java
@@ -68,9 +68,26 @@ public class Controller implements ActionListener {
                 }
                 break;
             case "Confirm":
-                int confirm = JOptionPane.showConfirmDialog(view, "Please double check inputs then press Yes to confirm.", "Booking confirmation", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-                if(confirm == JOptionPane.YES_OPTION){
-                    model.confirmBooking();
+                String name = view.nameField.getText();
+                String phNum = view.phNumField.getText();
+                String show = model.getShowID();
+                int gold = Integer.parseInt((String)view.goldQuant.getSelectedItem());
+                int silver = Integer.parseInt((String)view.silverQuant.getSelectedItem());
+                int bronze = Integer.parseInt((String)view.bronzeQuant.getSelectedItem());
+                
+                if(name.equalsIgnoreCase("") || name == null){
+                    JOptionPane.showMessageDialog(view, "Please input your name", "Error", JOptionPane.ERROR_MESSAGE);
+                } else if(phNum.length() < 9){
+                    JOptionPane.showMessageDialog(view, "Please input valid phone number", "Error", JOptionPane.ERROR_MESSAGE);
+                } else if(gold == 0 && silver == 0 && bronze == 0){
+                    JOptionPane.showMessageDialog(view, "Please choose valid ticket quantities", "Error", JOptionPane.ERROR_MESSAGE);
+                } else{
+                    String output = String.format("Name: %s\n Phone Number: %s\n Show: %s\n Gold: %d\n Silver: %d\n Bronze: %d",
+                            name, phNum, show, gold, silver, bronze);
+                    int confirm = JOptionPane.showConfirmDialog(view, output, "Booking confirmation", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+                    if (confirm == JOptionPane.YES_OPTION) {
+                        model.confirmBooking(name, phNum);
+                    }
                 }
                 break;
             case "Cancel":
